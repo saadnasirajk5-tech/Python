0634f187ffa1bb9f6e46df94f40a4b6e15424f2d
Saad Nasir <saadnasirajk5@gmail.com>
2026-03-12 22:22:43 +0500

    First two problems

---
+++ Functions/Basics & Parameters/probs_func.py
@@ -0,0 +1,34 @@
+""" 
+Write a function `add_numbers(a, b)` that returns the sum. 
+Then extend it to accept any number of positional arguments using `*args`."""
+def add_numbers(*args):
+    return sum(args)
+print(add_numbers(3,2,3,2))
+
+"""Write a function `greet(name="World")` that returns `"Hello, <name>!"`.
+   Test with and without passing a name."""
+
+def greet(name = "World"):
+    return "Hello, <name>!"       
+print(greet(name="ALI"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
